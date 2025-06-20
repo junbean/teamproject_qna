23db75c32d215bf0de85d8e8e5496cb72cd58113
junbean <junbin2438@naver.com>
2025-06-20 10:40:10 +0900

    프로젝트 생성 업로드

---
+++ teamproject_qna/src/main/java/com/example/teamproject_qna/TeamprojectQnaApplication.java
@@ -0,0 +1,13 @@
+package com.example.teamproject_qna;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TeamprojectQnaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TeamprojectQnaApplication.class, args);
+	}
+
+}
---
+++ teamproject_qna/src/test/java/com/example/teamproject_qna/TeamprojectQnaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.teamproject_qna;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TeamprojectQnaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
